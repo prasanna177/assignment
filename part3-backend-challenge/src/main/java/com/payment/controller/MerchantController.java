c5687755e8128b6f76983bc939506bb07bc457f1
prasanna177 <prasannachand77@gmail.com>
2025-11-23 23:23:06 +0500

    feat: added edit controller

--- part3-backend-challenge/src/main/java/com/payment/controller/MerchantController.java
+++ part3-backend-challenge/src/main/java/com/payment/controller/MerchantController.java
@@ -1,10 +1,13 @@
 package com.payment.controller;
 import com.payment.payload.merchants.MerchantAddPayload;
+import com.payment.payload.merchants.MerchantEditPayload;
 import com.payment.usecase.MerchantAddUseCase;
+import com.payment.usecase.MerchantEditUseCase;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Put;
 import jakarta.inject.Inject;
 import com.payment.RestResponse;
 import com.payment.payload.SearchRequestPayload;
@@ -16,11 +19,13 @@ public class MerchantController {
 
     private final MerchantListUseCase merchantListUseCase;
     private final MerchantAddUseCase merchantAddUseCase;
+    private  final MerchantEditUseCase merchantEditUseCase;
 
     @Inject
-    public MerchantController(MerchantListUseCase merchantUseCase,MerchantAddUseCase merchantAddUseCase) {
+    public MerchantController(MerchantListUseCase merchantUseCase,MerchantAddUseCase merchantAddUseCase, MerchantEditUseCase merchantEditUseCase) {
         this.merchantListUseCase = merchantUseCase;
         this.merchantAddUseCase = merchantAddUseCase;
+        this.merchantEditUseCase = merchantEditUseCase;
     }
 
     @Post("/search")
@@ -35,6 +40,15 @@ public class MerchantController {
     @Post
     public HttpResponse<RestResponse> addMerchant(@Body MerchantAddPayload request) {
         var response = this.merchantAddUseCase.execute(request);
+        if (response.isEmpty()) {
+            return HttpResponse.badRequest(RestResponse.error());
+        }
+        return HttpResponse.ok(RestResponse.success(response.get()));
+    }
+
+    @Put
+    public HttpResponse<RestResponse> updateMerchant(@Body MerchantEditPayload request) {
+        var response = this.merchantEditUseCase.execute(request);
         if (response.isEmpty()) {
             return HttpResponse.badRequest(RestResponse.error());
         }
