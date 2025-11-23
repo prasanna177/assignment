8647a6778b88029a785d393e6212c07c6649d76c
prasanna177 <prasannachand77@gmail.com>
2025-11-23 22:57:26 +0500

    enhance: added utils for generating id

--- part3-backend-challenge/src/main/java/com/payment/utils/HelperUtils.java
+++ part3-backend-challenge/src/main/java/com/payment/utils/HelperUtils.java
@@ -1,10 +1,30 @@
 package com.payment.utils;
 
 import java.util.Objects;
+import java.util.UUID;
 
 public class HelperUtils {
     public static long getStartingIndex(int pageNumber, int pageSize) {
         return Objects.nonNull(pageNumber) ?
                 (long) pageSize * ((long) pageNumber != 0 ? pageNumber - 1 : 0) : 0;
+    }
+
+    public static synchronized String generateUUID() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+
+    public static boolean isBlankOrNull(String str) {
+        int strLen;
+        if (str != null && (strLen = str.length()) != 0) {
+            for (int i = 0; i < strLen; ++i) {
+                if (!Character.isWhitespace(str.charAt(i))) {
+                    return false;
+                }
+            }
+
+            return true;
+        } else {
+            return true;
+        }
     }
 }
