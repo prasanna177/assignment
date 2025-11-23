cbc61b34533170d26791bc59e88e2af3252c1555
prasanna177 <prasannachand77@gmail.com>
2025-11-24 00:09:21 +0500

    feat: wrote business logic for merchant details

---
+++ part3-backend-challenge/src/main/java/com/payment/payload/merchants/MerchantDetailResponse.java
@@ -0,0 +1,73 @@
+package com.payment.payload.merchants;
+
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.serde.annotation.Serdeable;
+
+@Introspected
+@Serdeable
+public class MerchantDetailResponse {
+    String id;
+    String name;
+    String email;
+    String phone;
+    String status;
+    String address;
+    String businessName;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBusinessName() {
+        return businessName;
+    }
+
+    public void setBusinessName(String businessName) {
+        this.businessName = businessName;
+    }
+}
